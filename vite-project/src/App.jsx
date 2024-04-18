ac90fe131a8aa26d9a95f354b01e69b3c21e9a33
subash123r <rssubash96@gmail.com>
2024-04-18 13:29:18 +0530

    task  shop cart

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,37 @@
 
 import './App.css'
 
+
+export default function App() {
+  const [price, setPrice] = useState(5000);
+  const [storehouse, setStoreHouse] = useState(10);
+  const [cart, setCart] = useState(0);
+  function reducer() {}
+  const [state, dispatch] = useReducer(reducer, initialStore);
+  console.log(state);
+  return (
+    <div className="App">
+      {/* Setup provider subscriber method */}
+      <shopCtx.Provider value={{ state }}>
+        <ProductAdmin />
+        <UserComponents />
+        <CheckoutComponet />
+      </shopCtx.Provider>
+    </div>
+  );
+}
+
+function Cart() {
+  //use the context
+  const { state } = useContext(shopCtx);
+
+  function handleAddTocart() {
+    if (storehouse > 0) {
+      setCart(cart + 1);
+      setStoreHouse(storehouse - 1);
+    }
+  }
+}
 function App() {
 
   return (
@@ -9,7 +40,7 @@ function App() {
 <div className='headder'>
   <h1 className='start'>start bootstrap </h1>
   <ul>home / about / shop</ul>
-  <button className='btn'>🛒cart</button>
+  <button className='btn'>🛒cart {state.cart}</button>
 </div>
 <div className='shop'>
   <h1 className='style'>shop in style</h1>
@@ -25,7 +56,7 @@ function App() {
   <div className='pad'>
   <h3 className='h3_2'>Special Item</h3>
   <h4 className='h4_2'>$20.00 $18.00</h4>
-  <button className='btn_2'>add to cart</button>
+  <button className='btn_2'  onClick={handleAddTocart}>add to cart</button>
   </div>
 </div>
 
