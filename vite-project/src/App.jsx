4f37e50ed3a2fd39df83e41948acad038d02b170
subash123r <rssubash96@gmail.com>
2024-04-18 13:19:57 +0530

    shopping comp

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,107 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <hr />
+<div className='headder'>
+  <h1 className='start'>start bootstrap </h1>
+  <ul>home / about / shop</ul>
+  <button className='btn'>🛒cart</button>
+</div>
+<div className='shop'>
+  <h1 className='style'>shop in style</h1>
+  <h1 className='sstyle_2'>With this shop hompeage template</h1>
+</div>
+
+
+<div className='card'>
+
+<div className='img'><h1 className='num'>250 x 300</h1>
+
+</div>
+  <div className='pad'>
+  <h3 className='h3_2'>Special Item</h3>
+  <h4 className='h4_2'>$20.00 $18.00</h4>
+  <button className='btn_2'>add to cart</button>
+  </div>
+</div>
+
+
+<div className='card'>
+<div className='img'><button className='sale_btn'>sale</button><h1 className='num'>250 x 300</h1></div>
+  <div className='pad'>
+  <h3 className='h3_2'>Sale Item</h3>
+  <h4 className='h4_2'>$50.00 $25.00</h4>
+  <button className='btn_2'>add to cart</button>
+  </div>
+</div>
+
+
+
+<div className='card'>
+<div className='img'><button className='sale_btn'>sale</button><h1 className='num'>250 x 300</h1></div>
+  <div className='pad'>
+  <h3 className='h3_2'>Popular Item</h3>
+  <h4 className='h4_2'>$40.00</h4>
+  <button className='btn_2'>add to cart</button>
+  </div>
+</div>
+
+
+<div className='card'>
+<div className='img'><h1 className='num'>250 x 300</h1></div>
+  <div className='pad'>
+  <h3 className='h3_2'>Fancy Product</h3>
+  <h4 className='h4_2'>$40.00 - $80.00</h4>
+  <button className='btn_2'>view options</button>
+  </div>
+</div>
+
+
+<div className='card'>
+<div className='img'><button className='sale_btn'>sale</button><h1 className='num'>250 x 300</h1></div>
+  <div className='pad'>
+  <h3 className='h3_2'>Fancy Product</h3>
+  <h4 className='h4_2'>$40.00 - $80.00</h4>
+  <button className='btn_2'>view options</button>
+  </div>
+</div>
+
+<div className='card'>
+<div className='img'><h1 className='num'>250 x 300</h1></div>
+  <div className='pad'>
+  <h3 className='h3_2'>Fancy Product</h3>
+  <h4 className='h4_2'>$40.00 - $80.00</h4>
+  <button className='btn_2'>view options</button>
+  </div>
+</div>
+
+<div className='card'>
+<div className='img'><button className='sale_btn'>sale</button><h1 className='num'>250 x 300</h1></div>
+  <div className='pad'>
+  <h3 className='h3_2'>Fancy Product</h3>
+  <h4 className='h4_2'>$40.00 - $80.00</h4>
+  <button className='btn_2'>view options</button>
+  </div>
+</div>
+
+<div className='card'>
+<div className='img'><h1 className='num'>250 x 300</h1></div>
+  <div className='pad'>
+  <h3 className='h3_2'>Fancy Product</h3>
+  <h4 className='h4_2'>$40.00 - $80.00</h4>
+  <button className='btn_2'>view options</button>
+  </div>
+</div>
+
+    </>
+  )
+   
+}
+
+
+
+export default App
